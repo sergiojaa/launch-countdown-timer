f91fadc079bb5abacf47bf060313c66d87d345b4
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-07-28 00:18:45 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const days = document.getElementById('days')
+const hours = document.getElementById('hours')
+const minutes = document.getElementById('minutes')
+const seconds = document.getElementById('seconds')
+
+const targetDate = new Date().getTime() + 3600 * 1000 * 24 * 14;
